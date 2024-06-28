d2f6e339be083625bdee582558b1384022af62f6
Malik Muhammad Umair <167450073+umair9215@users.noreply.github.com>
2024-06-28 12:28:55 -0700

    FOR LOOP JS PRACTICAL  05

---
+++ script.js
@@ -0,0 +1,57 @@
+// let cars = ["Honda", "Civic", "Suzuki", "Alton", "Priush"];
+// cars.splice(0, 1, "MarkX", "Corolla", "Grande")
+// console.log(cars);
+
+// let names = ["Fahad", "Abdullah", "Rashid", "Hammad"];
+
+// brothers = names.slice(0, 3);
+// console.log(brothers);
+
+//  for (let i = 1; i <=50; i++) {
+//     console.log("Malik Muhammad Umair");
+//  }
+
+//  Assignment Number 01
+
+// let numbers = [1, 2, 3, 4, 5];
+// numbers.splice(0, 2);
+// // console.log(numbers);
+// numbers.splice(3, 3, 6, 7, 8);
+// console.log(numbers);
+
+// Assignment Number 02
+
+// let array = [1, 2, 3, 4, 5];
+//  for(let i = 1; i <2; i++) {
+//     console.log(array.indexOf(2));
+//     console.log(array.indexOf(4));
+//  }
+
+//  Assignment Number 03
+
+//    let array1 = [3, 4, 5];
+//    array1.splice(0, 0, 1, 2);
+// //    console.log(array);
+//    array1.splice(4, 1);
+//    for (let i = 0; i < 1; i++) {
+//     console.log(array1);
+//    }
+
+//  Assignment Number 04
+ let array2 = [1, 2, 3, 4, 5];
+ for (let i = 0; i < 5; i++) {
+        if (array2[i] > array2[i - 1]){
+                
+                console.log(array2[i]);
+         }
+ }
+
+
+//  Assignment Number 05
+
+// let array3 = [1, 2, 3, 4, 5];
+// array3.indexOf(3);
+// array3.splice(2, 3);
+// for (let i = 0; i < 1; i++) {
+//         console.log(array3);
+// }
