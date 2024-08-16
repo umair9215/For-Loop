c504e004d7e4796d3bbfd1b592ab84627691891f
Malik Muhammad Umair <167450073+umair9215@users.noreply.github.com>
2024-08-16 14:58:00 -0700

    Done Now

--- script.js
+++ script.js
@@ -22,29 +22,30 @@
 // Assignment Number 02
 
 // let array = [1, 2, 3, 4, 5];
-//  for(let i = 1; i <2; i++) {
-//     console.log(array.indexOf(2));
-//     console.log(array.indexOf(4));
-//  }
+// for(let i = 0; i <array.length; i++) {
+//   if (array[i]%2   === 0) {
+//        console.log(i);
+//   }
+// }
 
 //  Assignment Number 03
 
-//    let array1 = [3, 4, 5];
-//    array1.splice(0, 0, 1, 2);
-// //    console.log(array);
-//    array1.splice(4, 1);
-//    for (let i = 0; i < 1; i++) {
-//     console.log(array1);
-//    }
+   let array1 = [3, 4, 5];
+   array1.splice(0, 0, 1, 2);
+//    console.log(array);
+   array1.splice(4, 1);
+   for (let i = 0; i < 1; i++) {
+    console.log(array1);
+   }
 
 //  Assignment Number 04
- let array2 = [1, 2, 3, 4, 5];
- for (let i = 0; i < 5; i++) {
-        if (array2[i] > array2[i - 1]){
+//  let array2 = [1, 2, 3, 4, 5];
+//  for (let i = 0; i < 5; i++) {
+//         if (array2[i] > array2[i - 1]){
                 
-                console.log(array2[i]);
-         }
- }
+//                 console.log(array2[i]);
+//          }
+//  }
 
 
 //  Assignment Number 05
